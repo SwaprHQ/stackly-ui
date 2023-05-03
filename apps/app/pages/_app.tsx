fc896c547ada269d9a2fa8e82546a0c30380b341
Wixzi <wixzi@pm.me>
2023-05-03 09:45:44 +0200

    chore: Remove example layout

--- apps/app/pages/_app.tsx
+++ apps/app/pages/_app.tsx
@@ -1,14 +1,8 @@
-import type { AppProps } from 'next/app'
-import type { LayoutProps } from '@vercel/examples-ui/layout'
-import { getLayout } from '@vercel/examples-ui'
-import '@vercel/examples-ui/globals.css'
+import type { AppProps } from "next/app";
+import "@vercel/examples-ui/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const Layout = getLayout<LayoutProps>(Component)
+  
 
-  return (
-    <Layout title="Monorepo" path="solutions/monorepo">
-      <Component {...pageProps} />
-    </Layout>
-  )
+  return <Component {...pageProps} />;
 }
