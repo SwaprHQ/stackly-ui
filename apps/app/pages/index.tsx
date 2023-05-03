e0a402f2b21ca364ec8d81905a1cc8407d0065cc
Wixzi <wixzi@pm.me>
2023-05-03 09:15:01 +0200

    Initial commit

---
+++ apps/app/pages/index.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+import { Layout, Page, Text, List, Code } from '@vercel/examples-ui'
+import { Button } from '@acme/ui'
+import { matchingTextColor, randomColor } from '@acme/utils'
+
+export default function Index() {
+  const [bgColor, setBgColor] = useState('')
+  const [textColor, setTextColor] = useState('')
+  const changeColor = () => {
+    const bg = randomColor()
+    setBgColor(bg)
+    setTextColor(matchingTextColor(bg))
+  }
+
+  useEffect(changeColor, [])
+
+  return (
+    <Page>
+      <Text variant="h1" className="mb-6">
+        Hello Stackly UI
+      </Text>
+    
+      {bgColor && textColor && (
+        <>
+          <Button
+            style={{
+              backgroundColor: bgColor,
+              color: textColor,
+              borderColor: textColor,
+            }}
+            onClick={changeColor}
+          >
+            Stackly Color Button
+          </Button>
+        </>
+      )}
+    </Page>
+  )
+}
+
+Index.Layout = Layout
