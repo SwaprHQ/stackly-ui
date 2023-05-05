90667d432bcf01dd7ebb75038c8523e1d8a31cb9
Wixzi <wixzi@pm.me>
2023-05-05 08:02:02 +0200

    build: Clean up code and packages

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -2,7 +2,9 @@ import type { AppProps } from "next/app";
 import "@vercel/examples-ui/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  
-
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps}/>
+    </>
+  );
 }
