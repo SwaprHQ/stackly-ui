32c3d7867d7e6cb8a564bbcea6ed9bbfa3468f6d
Wixzi <wixzi@pm.me>
2023-05-05 09:10:50 +0200

    feat: Fix Button UI

---
+++ packages/ui/src/Button/Button.tsx
@@ -0,0 +1,23 @@
+import type { ButtonHTMLAttributes } from "react";
+import cn from "clsx";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function Button({ children, className, ...props }: ButtonProps) {
+  const rootClassName = cn(
+    "relative inline-flex items-center justify-center cursor-pointer",
+    "no-underline py-0 px-3.5 rounded-md border border-solid border-black",
+    "bg-black text-white text-base font-medium outline-none select-none",
+    "align-middle whitespace-nowrap leading-10 shadow-md transition-colors",
+    className
+  );
+
+  return (
+    <button className={rootClassName} {...props}>
+      {children}
+    </button>
+  );
+}
