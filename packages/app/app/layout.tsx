52f7f66df711cdecf79a8bea2162300d8e11f8b8
Wixzi <wixzi@pm.me>
2023-05-09 12:55:55 +0200

    feat: Updated layout file

--- packages/app/app/layout.tsx
+++ packages/app/app/layout.tsx
@@ -14,33 +14,6 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/assets/favicons/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/assets/favicons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/assets/favicons/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/assets/favicons/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/assets/favicons/safari-pinned-tab.svg"
-          color="#F8F8F7"
-        />
-        <meta name="msapplication-TileColor" content="#F8F8F7" />
-        <meta name="theme-color" content="#A2E771" />
-      </head>
       <body>{children}</body>
     </html>
   );
