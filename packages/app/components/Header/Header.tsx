b512e0e73148170f95f22080dbfc320902e9f2ee
Wixzi <wixzi@pm.me>
2023-05-10 22:04:06 +0200

    feat: [STK-62] Mobile view

--- packages/app/components/Header/Header.tsx
+++ packages/app/components/Header/Header.tsx
@@ -14,11 +14,18 @@ export default function Header() {
               aria-label="Stackly"
               src={Stackly}
               alt="Stackly Logo"
-              className="ml-[12px]"
+              className="ml-[12px] hidden md:block"
             />
           </Link>
         </div>
-        <div className="border-r border-solid border-b-gray-100 h-[31px] ml-[40px]"></div>
+        <div className="border-r border-solid border-b-gray-100 h-[31px] ml-[20px] md:ml-[40px]"></div>
+        <div className="flex justify-end items-center gap-4 w-full md:hidden">
+          <button className="space-y-1.5 md:invisible px-[12px] py-[12px] rounded-md  bg-[#F3F4F2] active:bg-[#ECEDEB]">
+            <div className="w-[15px] h-0.5 bg-gray-900 "></div>
+            <div className="w-[15px] h-0.5 bg-gray-900"></div>
+            <div className="w-[15px] h-0.5 bg-gray-900"></div>
+          </button>
+        </div>
       </nav>
     </header>
   );
