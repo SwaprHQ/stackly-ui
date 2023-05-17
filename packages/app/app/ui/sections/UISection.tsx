71b4d860447eec7dfbeb3c64f383de1a0228f8f9
Diogo Ferreira <diogomf@hey.com>
2023-05-17 23:08:22 +0100

    feat: add icons section to ui page

--- packages/app/app/ui/sections/UISection.tsx
+++ packages/app/app/ui/sections/UISection.tsx
@@ -9,7 +9,7 @@ export interface SectionProps {
 export const UISection = ({ title, children, description }: SectionProps) => (
   <div className="pb-12 my-12 space-y-5 border-b">
     <div className="space-y-1">
-      <h2 className="text-lg font-bold">{title}</h2>
+      <h2 className="text-2xl font-medium">{title}</h2>
       {description && <p className="text-em-med">{description}</p>}
     </div>
     {children}
