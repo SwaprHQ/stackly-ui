8735e1f1d6f85b664079d95e19cbb114297b4caa
Berteotti <berteotti2@gmail.com>
2023-05-25 10:35:03 +0100

    feat: create ChipButton and RadioButton elements
    - Add tailwind-merge

--- packages/app/app/ui/page.tsx
+++ packages/app/app/ui/page.tsx
@@ -2,9 +2,19 @@
 
 import { UISection } from "@/app/ui/sections/UISection";
 import { UISubSection } from "@/app/ui/sections/UISubSection";
-import { Button, ButtonLink, Icon, IconName, iconMap } from "@/ui";
+import {
+  Button,
+  ButtonLink,
+  Icon,
+  IconName,
+  iconMap,
+  ChipButton,
+  RadioButton,
+} from "@/ui";
+import { useState } from "react";
 
 export default function Page() {
+  const [activeButton, setActiveButton] = useState("0");
   return (
     <div className="my-10">
       <h1 className="text-3xl font-medium">Stackly UI</h1>
@@ -12,7 +22,7 @@ export default function Page() {
         title="Buttons"
         description="This is a collection of all our current buttons divided by actions."
       >
-        <UISubSection title="primary">
+        <UISubSection title="Primary">
           <Button size="lg" onClick={() => console.log("hey")}>
             Try Stackly now
           </Button>
@@ -28,13 +38,13 @@ export default function Page() {
             active={true}
             onClick={() => console.log("hey")}
           >
-            active
+            Active
           </Button>
           <Button size="sm" disabled={true} onClick={() => console.log("hey")}>
-            button disabled
+            Button disabled
           </Button>
         </UISubSection>
-        <UISubSection title="secondary">
+        <UISubSection title="Secondary">
           <Button
             action="secondary"
             iconLeft="plus"
@@ -50,10 +60,10 @@ export default function Page() {
             onClick={() => console.log("hey")}
           ></Button>
           <ButtonLink action="secondary" size="sm" href="/">
-            soy un ButtonLink
+            Soy un ButtonLink
           </ButtonLink>
         </UISubSection>
-        <UISubSection title="tertiary">
+        <UISubSection title="Tertiary">
           <Button
             action="tertiary"
             iconLeft="plus"
@@ -71,7 +81,7 @@ export default function Page() {
             Cancel
           </Button>
         </UISubSection>
-        <UISubSection title="quaternary">
+        <UISubSection title="Quaternary">
           <Button
             action="quaternary"
             iconLeft="blocks"
@@ -99,6 +109,69 @@ export default function Page() {
               <p className="p-1 text-xs rounded-lg bg-surface-75">{iconName}</p>
             </div>
           ))}
+        </div>
+      </UISection>
+      <UISection title="Chip Buttons">
+        <div className="flex flex-wrap space-x-4">
+          <ChipButton
+            active={true}
+            onClick={() => {
+              console.log("I'm active");
+            }}
+          >
+            Active
+          </ChipButton>
+          <ChipButton
+            active={false}
+            onClick={() => {
+              console.log("I'm inactive");
+            }}
+          >
+            Inactive
+          </ChipButton>
+          <ChipButton onClick={() => console.log(" Eu tenho um icon")}>
+            <Icon name="blocks" />
+            <span>Eu tenho um icon</span>
+          </ChipButton>
+          <ChipButton onClick={() => console.log(" Eu tenho um icon")}>
+            Hour
+          </ChipButton>
+          <ChipButton
+            onClick={() => console.log("Eu tenho um icon")}
+            disabled={true}
+          >
+            SWPR
+          </ChipButton>
+        </div>
+      </UISection>
+      <UISection title="Radio Buttons">
+        <div className="flex flex-wrap space-x-4">
+          <RadioButton
+            name="test"
+            id="first"
+            checked={activeButton === "0"}
+            value={"0"}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setActiveButton(e.target.value);
+              console.log("I'm active");
+            }}
+          >
+            Active
+          </RadioButton>
+          <RadioButton
+            name="test"
+            id="second"
+            checked={activeButton === "1"}
+            value={"1"}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setActiveButton(e.target.value);
+              console.log("I'm inactive");
+            }}
+          >
+            Inactive
+          </RadioButton>
         </div>
       </UISection>
     </div>
