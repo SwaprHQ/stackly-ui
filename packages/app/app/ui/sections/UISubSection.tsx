bb5a27adbb40a9995eca096e183bffd10453163b
Diogo Ferreira <diogomf@hey.com>
2023-05-30 18:01:46 +0100

    feat: update UI Sections to use text components

--- packages/app/app/ui/sections/UISubSection.tsx
+++ packages/app/app/ui/sections/UISubSection.tsx
@@ -1,4 +1,5 @@
 import { SectionProps } from "@/app/ui/sections/UISection";
+import { HeadingText } from "@/ui";
 
 export const UISubSection = ({
   title,
@@ -7,7 +8,7 @@ export const UISubSection = ({
 }: SectionProps) => (
   <div className="space-y-4">
     <div className="space-y-1">
-      <h3 className="font-semibold">{title}</h3>
+      <HeadingText variant={6}>{title}</HeadingText>
       {description && <p className="text-sm text-em-med">{description}</p>}
     </div>
     <div className="flex flex-wrap items-start space-x-6 space-y-3 lg:space-y-2 xl:space-y-0">
