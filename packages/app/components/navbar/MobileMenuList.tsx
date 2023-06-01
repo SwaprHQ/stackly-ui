9a4c1d448b0c0d709b49bc8cf0d49a8ab2972b21
Diogo Ferreira <diogomf@hey.com>
2023-06-01 18:08:47 +0100

    fix: add link on mobile nav to  stacks

--- packages/app/components/navbar/MobileMenuList.tsx
+++ packages/app/components/navbar/MobileMenuList.tsx
@@ -16,7 +16,7 @@ export default function MobileMenuList({ open, toggle }: MobileMenuListProps) {
         onClick={toggle}
       ></div>
       <div className="absolute left-0 w-full gap-2 px-6 py-2 border-b border-solid rounded-lg bg-surface-25 top-nav-height border-surface-75">
-        <Link href="#" className="flex items-center py-3 text-em-med">
+        <Link href="/stacks" className="flex items-center py-3 text-em-med">
           <Icon name="blocks" alt="your stacks" size={18} />
           <span className="ml-4">Your Stacks</span>
         </Link>
