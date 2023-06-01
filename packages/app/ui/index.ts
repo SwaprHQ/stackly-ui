cbb32ef43e34ed840bb9ed9c7d651131c20fde6d
Diogo Ferreira <diogomf@hey.com>
2023-06-01 13:39:51 +0100

    update: change location and imports of ModalHeaderTitle

--- packages/app/ui/index.ts
+++ packages/app/ui/index.ts
@@ -1,5 +1,4 @@
 export * from "./buttons";
 export * from "./icon/Icon";
 export * from "./modal";
-export * from "./modal-header-title/ModalHeaderTitle";
 export * from "./dialog";
--- packages/app/ui/modal/index.ts
+++ packages/app/ui/modal/index.ts
@@ -2,3 +2,4 @@ export * from "./Modal";
 export * from "./ModalHeader";
 export * from "./ModalContent";
 export * from "./ModalFooter";
+export * from "./ModalHeaderTitle";
