ba5376315f2b7079ca253b9b2510cbc6c067b464
Diogo Ferreira <diogomf@hey.com>
2023-06-02 11:25:09 +0100

    fix: toggle menu when clicking a mobile link

--- packages/app/components/navbar/MobileMenuList.tsx
+++ packages/app/components/navbar/MobileMenuList.tsx
@@ -16,7 +16,11 @@ export default function MobileMenuList({ open, toggle }: MobileMenuListProps) {
         onClick={toggle}
       ></div>
       <div className="absolute left-0 w-full gap-2 px-6 py-2 border-b border-solid rounded-lg bg-surface-25 top-nav-height border-surface-75">
-        <Link href="/stacks" className="flex items-center py-3 text-em-med">
+        <Link
+          href="/stacks"
+          className="flex items-center py-3 text-em-med"
+          onClick={toggle}
+        >
           <Icon name="blocks" alt="your stacks" size={18} />
           <span className="ml-4">Your Stacks</span>
         </Link>
