547199831beb0c0575d924003f28ce75c63b4eee
Diogo Ferreira <diogomf@hey.com>
2023-06-05 12:37:23 +0100

    feat: add Your stacks header and tabs (#18)
    * feat: add Your stacks header and tabs
    - add header with title and button
    - add tabs with headless ui
    - install @headlessui/tailwindcss plugin to be able to use "selected-ui:" helpers
    - add layout page
    - add mock stacks table
    
    * fix: use PropsWithChildren on layouts

---
+++ packages/app/app/stacks/layout.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from "react";
+
+export const metadata = {
+  title: "Your stacks | Stackly",
+};
+
+export default function YourStacksLayout({ children }: PropsWithChildren) {
+  return <div className="max-w-5xl pt-12 mx-auto md:pt-24">{children}</div>;
+}
---
+++ packages/app/app/stacks/table.tsx
@@ -0,0 +1,5 @@
+export const StacksTable = ({ text }: { text: string }) => (
+  <div className="w-full bg-white h-96 rounded-3xl">
+    <p className="py-32 text-center text-em-low">{text} table</p>
+  </div>
+);
