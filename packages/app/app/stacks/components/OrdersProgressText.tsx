f807fc41fcfff611b75cbe07591e3817fe85084c
Diogo Ferreira <diogomf@hey.com>
2023-06-15 11:40:22 +0100

    feat: add Stackstable base (with mockdata) (#19)

---
+++ packages/app/app/stacks/components/OrdersProgressText.tsx
@@ -0,0 +1,18 @@
+import { ordersDone } from "@/app/stacks/components/StacksTable";
+import { Order } from "@/app/stacks/page";
+import { BodyText } from "@/ui";
+import { formatTimestampToDate } from "@/utils/time";
+
+export const OrdersProgressText = ({ order }: { order: Order }) =>
+  ordersDone(order) === 0 ? (
+    <BodyText className="text-primary-700">
+      Starts on {formatTimestampToDate(order.orderSlots[0])}
+    </BodyText>
+  ) : (
+    <>
+      <BodyText className="text-em-high">
+        {ordersDone(order).toString()}
+      </BodyText>
+      <BodyText className="text-em-low">{`/ ${order.orderSlots.length} orders`}</BodyText>
+    </>
+  );
