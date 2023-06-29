6936e821cb12c3fe7bfe6202aefcf2be014cc0a6
Diogo Ferreira <diogomf@hey.com>
2023-06-29 15:18:49 +0100

    update: add StackOrderProps and change cow type from OrderMetaData to Order

--- packages/app/models/stack-order/types.ts
+++ packages/app/models/stack-order/types.ts
@@ -1,6 +1,10 @@
 import { Order } from "@/models/order";
-import { OrderMetaData } from "@cowprotocol/cow-sdk";
+import { Order as CowOrder } from "@cowprotocol/cow-sdk";
 
 export interface StackOrder extends Order {
-  cowData?: OrderMetaData[];
+  cowOrders: CowOrder[];
+}
+
+export interface StackOrderProps {
+  stackOrder: StackOrder;
 }
