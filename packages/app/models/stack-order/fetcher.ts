265572075a130995f624b72cd4e06ff66991a6db
Diogo Ferreira <diogomf@hey.com>
2023-07-03 20:16:15 +0200

    update: simplify promises fetcher

--- packages/app/models/stack-order/fetcher.ts
+++ packages/app/models/stack-order/fetcher.ts
@@ -4,20 +4,9 @@ import { StackOrder } from "@/models/stack-order/types";
 
 export async function getStackOrders(orders: Order[]): Promise<StackOrder[]> {
   const ordersPromises = orders.map(async (order): Promise<StackOrder> => {
-    try {
-      const cowOrders = await getCowOrders(order.id);
-      return { ...order, cowOrders };
-    } catch (error) {
-      console.error("Error on getStackOrders", error);
-      throw error; // Re-throw the error to propagate it to Promise.all
-    }
+    const cowOrders = await getCowOrders(order.id);
+    return { ...order, cowOrders };
   });
 
-  try {
-    return await Promise.all(ordersPromises);
-  } catch (error) {
-    // Handle any errors thrown during Promise.all
-    console.error("Error on stack orders promise.all", error);
-    throw error;
-  }
+  return await Promise.all(ordersPromises);
 }
