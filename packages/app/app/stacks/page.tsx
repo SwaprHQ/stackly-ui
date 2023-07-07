e93b70a201d96e470599c8e70d6b7b83bd88e277
Diogo Ferreira <diogomf@hey.com>
2023-07-07 12:57:38 +0200

    feat: add empty state when no stackOrders active or complete

--- packages/app/app/stacks/page.tsx
+++ packages/app/app/stacks/page.tsx
@@ -2,7 +2,7 @@
 
 import { Tab } from "@headlessui/react";
 import { EmptyState } from "@/app/stacks/empty-state";
-import { ButtonLink, HeadingText } from "@/ui";
+import { BodyText, ButtonLink, HeadingText } from "@/ui";
 import { StacksTable } from "@/components/StacksTable";
 import { getOrders } from "@/models/order";
 import {
@@ -23,6 +23,11 @@ export default async function Page() {
   const mockOrders = await getOrders("address");
   if (!mockOrders) return <EmptyState />;
   const stackOrders = await getStackOrders(chainId, mockOrders);
+
+  const completedOrders = filterCompletedOrders(stackOrders);
+  const hasCompletedOrders = completedOrders.length > 0;
+  const activeOrders = filterActiveOrders(stackOrders);
+  const hasActiveOrders = activeOrders.length > 0;
 
   return (
     <div className="space-y-8">
@@ -58,13 +63,17 @@ export default async function Page() {
           </Tab.List>
           <Tab.Panels>
             <Tab.Panel>
-              {stackOrders && (
-                <StacksTable stackOrders={filterActiveOrders(stackOrders)} />
+              {hasActiveOrders ? (
+                <StacksTable stackOrders={activeOrders} />
+              ) : (
+                <EmptyStacks text=" No active stacks" />
               )}
             </Tab.Panel>
             <Tab.Panel>
-              {stackOrders && (
-                <StacksTable stackOrders={filterCompletedOrders(stackOrders)} />
+              {hasCompletedOrders ? (
+                <StacksTable stackOrders={completedOrders} />
+              ) : (
+                <EmptyStacks text="No completed stacks" />
               )}
             </Tab.Panel>
           </Tab.Panels>
@@ -73,3 +82,9 @@ export default async function Page() {
     </div>
   );
 }
+
+const EmptyStacks = ({ text }: { text: string }) => (
+  <div className="py-12 my-12 bg-white rounded-xl">
+    <BodyText className="text-center text-em-low">{text}</BodyText>
+  </div>
+);
