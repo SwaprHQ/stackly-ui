a6ef5ace644ffcc7e9b0e5a9cbf8ec7298f1ecf9
Rorry <rodrigojali@gmail.com>
2023-07-10 13:54:23 -0300

    feat(STK-88): implement token picker search and clear functionality, with empty state

--- packages/app/components/token-picker/TokenPicker.tsx
+++ packages/app/components/token-picker/TokenPicker.tsx
@@ -4,6 +4,7 @@ import { useTokenListContext } from "@/context/TokenListContext";
 import { Token } from "@/models/token/types";
 import {
   BodyText,
+  Button,
   Icon,
   Modal,
   ModalBaseProps,
@@ -11,9 +12,17 @@ import {
   ModalHeaderTitle,
   RadioButton
 } from "@/ui";
+import { ChangeEvent, useEffect, useState } from "react";
 import { TokenIcon } from "../TokenIcon";
 import { TOKEN_PICKER_COMMON_TOKENS } from "./constants";
+import EmptyStateImg from "public/assets/images/empty-state-stacks-coins.svg";
 
+const HALF_SECOND = 500;
+
+interface SearchBarProps {
+  onSearch: (event: ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+}
 interface TokenListRowProps {
   token: Token;
   closeAction: () => void;
@@ -21,29 +30,63 @@ interface TokenListRowProps {
 
 interface TokenListProps {
   closeAction: () => void;
+  onClearSearchClick: () => void;
   tokenList: Token[];
+  tokenSearchQuery?: string;
 }
 
 export const TokenPicker = ({ isOpen, closeAction }: ModalBaseProps) => {
+  const [tokenSearchQuery, setTokenSearchQuery] = useState("");
+  const [debouncedQuery, setDebouncedQuery] = useState(tokenSearchQuery);
+
   const { tokenList } = useTokenListContext();
+
+  const tokenListClearSearchCleanup = () => {
+    setDebouncedQuery("");
+    setTokenSearchQuery("");
+  };
+
+  const handleTokenSearchInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setDebouncedQuery(event.target.value);
+  };
+
+  /**
+   * Updates the token query value after 0.5s from the last
+   * keystroke of 'debouncedTerm'
+   */
+  useEffect(() => {
+    const timer = setTimeout(
+      () => setTokenSearchQuery(debouncedQuery),
+      HALF_SECOND
+    );
+
+    return () => clearTimeout(timer);
+  }, [debouncedQuery]);
 
   return (
     <Modal closeAction={closeAction} isOpen={isOpen}>
       <ModalHeaderTitle closeAction={closeAction} title="Select a token" />
       <ModalContent>
-        <SearchBar />
+        <SearchBar onSearch={handleTokenSearchInput} value={debouncedQuery} />
         <CommonTokens />
-        <TokenList closeAction={closeAction} tokenList={tokenList} />
+        <TokenList
+          closeAction={closeAction}
+          onClearSearchClick={tokenListClearSearchCleanup}
+          tokenList={tokenList}
+          tokenSearchQuery={tokenSearchQuery}
+        />
       </ModalContent>
     </Modal>
   );
 };
 
-const SearchBar = () => (
+const SearchBar = ({ onSearch, value }: SearchBarProps) => (
   <div className="flex items-center bg-surface-50 border border-surface-75 rounded-xl py-2 px-3 text-em-low">
     <Icon className="text-em-med" name="search" size={18} />
     <input
       className="outline-none font-semibold flex-grow ml-2 bg-surface-50"
+      onChange={onSearch}
+      value={value}
       placeholder="Search token name or paste address"
       type="text"
     />
@@ -92,17 +135,58 @@ const TokenListRow = ({ closeAction, token }: TokenListRowProps) => (
   </div>
 );
 
-const TokenList = ({ closeAction, tokenList }: TokenListProps) => (
-  <div
-    className="mt-5 overflow-y-auto h-72 border-t border-surface-50 divide-y divide-surface-50"
-    id="tokenPickerList"
-  >
-    {tokenList.map(token => (
-      <TokenListRow
-        closeAction={closeAction}
-        key={token.address}
-        token={token}
-      />
-    ))}
-  </div>
-);
+const TokenList = ({
+  closeAction,
+  onClearSearchClick,
+  tokenList,
+  tokenSearchQuery
+}: TokenListProps) => {
+  const [filteredTokenList, setFilteredTokenList] = useState<Token[]>(
+    tokenList
+  );
+
+  const handleClearSearchClick = () => {
+    onClearSearchClick();
+    setFilteredTokenList(tokenList);
+  };
+
+  useEffect(() => {
+    if (tokenSearchQuery) {
+      const filteredItems = tokenList.filter(
+        token =>
+          token.symbol.toLowerCase().includes(tokenSearchQuery.toLowerCase()) ||
+          token.name.toLowerCase().includes(tokenSearchQuery.toLowerCase())
+      );
+
+      setFilteredTokenList(filteredItems);
+    } else {
+      setFilteredTokenList(tokenList);
+    }
+  }, [tokenList, tokenSearchQuery]);
+
+  return (
+    <div className="mt-5 overflow-y-auto h-72 border-t border-surface-50 divide-y divide-surface-50">
+      {filteredTokenList.length
+        ? filteredTokenList.map(token => (
+            <TokenListRow
+              closeAction={closeAction}
+              key={token.address}
+              token={token}
+            />
+          ))
+        : tokenSearchQuery && (
+            <div className="flex items-center justify-center flex-col mt-8 space-y-4">
+              <EmptyStateImg />
+              <BodyText>{`Nothing found for "${tokenSearchQuery}"`}</BodyText>
+              <Button
+                action="secondary"
+                onClick={handleClearSearchClick}
+                size="md"
+              >
+                Clear search
+              </Button>
+            </div>
+          )}
+    </div>
+  );
+};
