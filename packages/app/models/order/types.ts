d52579034c7bef8b9a8479eac8c8e09eb843ee88
Diogo Ferreira <diogomf@hey.com>
2023-07-13 14:03:39 +0200

    fix: add missing denpendency of @stackly/sdk to package/app

--- packages/app/models/order/types.ts
+++ packages/app/models/order/types.ts
@@ -1,8 +1,6 @@
-import { Token } from "@/models/token/types";
 import { Order as StacklyOrder } from "@stackly/sdk/dist/vaults/subgraph";
 
-export type Order = StacklyOrder;
-
+export interface Order extends StacklyOrder {}
 export interface OrderProps {
   order: Order;
 }
