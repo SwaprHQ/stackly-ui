0961b8f4911a3a2ceae80bd4ffd49b1b80a50088
Rorry <rodrigojali@gmail.com>
2023-07-13 16:03:48 -0300

    feat(STK-89): add size variants for ChipButton

--- packages/app/ui/buttons/ChipButton.tsx
+++ packages/app/ui/buttons/ChipButton.tsx
@@ -1,48 +1,58 @@
 "use client";
 
 import { ButtonHTMLAttributes } from "react";
-import { buttonStyles } from "./base";
-import { cva, cx } from "class-variance-authority";
+import { cva } from "class-variance-authority";
 import { twMerge } from "tailwind-merge";
+
+import { buttonStyles, SizeProps } from "./base";
 
 interface ChipButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   active?: boolean;
+  size?: SizeProps;
 }
 
 const chipButtonStyles = cva(["rounded-full px-3 py-2 space-x-1.5 text-xs"], {
   variants: {
     active: {
-      true: ["bg-gray-75"],
+      true: ["bg-gray-75"]
     },
+    size: {
+      icon: "p-2",
+      lg: "px-14 py-3 md:py-4 text-lg md:px-24 space-x-4",
+      md: "px-8 md:px-12 py-2 space-x-2",
+      sm: "px-4 py-2 text-sm space-x-2"
+    }
   },
+  defaultVariants: {
+    size: "sm"
+  }
 });
 
 export function ChipButton({
+  active,
   children,
   className,
   disabled,
-  onClick,
-  active,
   id,
+  onClick,
+  size
 }: ChipButtonProps) {
   return (
     <button
+      className={twMerge(
+        buttonStyles({
+          size,
+          action: "tertiary",
+          width: null,
+          disabled,
+          active,
+          className
+        }),
+        chipButtonStyles({ active, size })
+      )}
+      disabled={disabled}
       id={id}
       onClick={onClick}
-      disabled={disabled}
-      className={twMerge(
-        cx(
-          buttonStyles({
-            size: "md",
-            action: "tertiary",
-            width: null,
-            disabled,
-            active,
-            className,
-          }),
-          chipButtonStyles({ active })
-        )
-      )}
     >
       {children}
     </button>
