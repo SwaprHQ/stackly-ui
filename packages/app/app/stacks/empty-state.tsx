3d42ab3c39d697768d797130d33a0eecfd66f9f1
ElRodrigote <rodrigo.ali@nuant.com>
2023-07-24 15:50:18 -0400

    refactor(STK-90): improve app scaffolding and client/server components for async components

--- packages/app/app/stacks/empty-state.tsx
+++ packages/app/app/stacks/empty-state.tsx
@@ -1,22 +1,24 @@
-import StackOfCoinsImage from "@/public/assets/images/empty-state-stacks-coins.svg";
+import { EmptyStateStacksImg } from "@/public/assets";
 import { ButtonLink, HeadingText } from "@/ui";
 
-export const EmptyState = () => (
-  <div className="max-w-xl mx-auto space-y-8">
-    <div className="flex flex-col items-center space-y-6 md:space-y-8">
-      <StackOfCoinsImage />
-      <div className="space-y-3 text-center">
-        <HeadingText size={2}>
-          {"Looks like you haven't created any stacks yet."}
-        </HeadingText>
-        <HeadingText size={1} weight="medium" className="text-em-med">
-          Simply click the button below to create your first stack. Once you
-          have created a stack, you can view its progress here.
-        </HeadingText>
+export default function EmptyState() {
+  return (
+    <div className="max-w-xl mx-auto space-y-8">
+      <div className="flex flex-col items-center space-y-6 md:space-y-8">
+        <EmptyStateStacksImg />
+        <div className="space-y-3 text-center">
+          <HeadingText size={2}>
+            {"Looks like you haven't created any stacks yet."}
+          </HeadingText>
+          <HeadingText size={1} weight="medium" className="text-em-med">
+            Simply click the button below to create your first stack. Once you
+            have created a stack, you can view its progress here.
+          </HeadingText>
+        </div>
       </div>
+      <ButtonLink href="/" iconLeft="plus" width="fit" className="mx-auto">
+        Create stack
+      </ButtonLink>
     </div>
-    <ButtonLink href="/" iconLeft="plus" width="fit" className="mx-auto">
-      Create stack
-    </ButtonLink>
-  </div>
-);
+  );
+}
