c11aa717866eaee9794ca6848d2a67fb7fafe97a
ElRodrigote <rodrigo.ali@nuant.com>
2023-07-27 23:34:57 -0400

    chore(STK-90): revert unwanted changes from this branch. Will be done by Diogo

--- packages/app/components/index.ts
+++ packages/app/components/index.ts
@@ -1,7 +1,6 @@
 import { ConnectButton } from "./ConnectButton";
 import { Navbar } from "./navbar/Navbar";
 import { SelectNetwork } from "./SelectNetwork";
-import { StackTabs } from "./stack-tabs/StackTabs";
 import TokenPicker from "./token-picker/TokenPicker";
 
 export * from "./stackbox";
@@ -10,4 +9,4 @@ export * from "./StacksTable";
 export * from "./TokenIcon";
 export * from "./DatePicker";
 
-export { ConnectButton, Navbar, SelectNetwork, StackTabs, TokenPicker };
+export { ConnectButton, Navbar, SelectNetwork, TokenPicker };
