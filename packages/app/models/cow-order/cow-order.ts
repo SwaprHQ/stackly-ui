d911e22f01b668680b63b948a986bcbc124302cb
Diogo Ferreira <diogomf@hey.com>
2023-07-28 13:02:44 +0100

    fix: use chainId from SDK

--- packages/app/models/cow-order/cow-order.ts
+++ packages/app/models/cow-order/cow-order.ts
@@ -1,15 +1,15 @@
-type ChainId = 1 | 100;
+import { ChainId } from "@stackly/sdk";
 
 export const COW_API_BASE_URL: Readonly<Record<ChainId, string>> = {
   1: `https://api.cow.fi/mainnet/api/v1`,
-  100: `https://api.cow.fi/xdai/api/v1`
+  100: `https://api.cow.fi/xdai/api/v1`,
 };
 
 const COW_EXPLORER_BASE_URL = "https://explorer.cow.fi/";
 
 export const COW_API_EXPLORER_URL: Readonly<Record<ChainId, string>> = {
   1: COW_EXPLORER_BASE_URL + "orders/",
-  100: COW_EXPLORER_BASE_URL + "gc/orders/"
+  100: COW_EXPLORER_BASE_URL + "gc/orders/",
 };
 
 export const cowExplorerUrl = (chainId: ChainId, uid: string) =>
