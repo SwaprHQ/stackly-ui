e6486109b8eb82c54edb3f866a4a7633fcb03e9c
Rorry <rodrigojali@gmail.com>
2023-08-04 12:19:42 -0300

    chore(STK-90): revert Stackbox container style changes for descriptiveness purposes

--- packages/app/app/page.tsx
+++ packages/app/app/page.tsx
@@ -1,5 +1,9 @@
 import { Stackbox } from "@/components";
 
 export default function Home() {
-  return <Stackbox />;
+  return (
+    <div className="w-full px-4 mx-auto">
+      <Stackbox />
+    </div>
+  );
 }
