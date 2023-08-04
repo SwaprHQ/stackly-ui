e7f3aaf33e0ca7946be584b86a3ec8f4c6bd5c58
Diogo Ferreira <diogomf@hey.com>
2023-08-04 13:28:14 +0100

    update: DialogConfirmTransactionLoadingProps to support passing children and optional close action

--- packages/app/components/DialogConfirmTransactionLoading.tsx
+++ packages/app/components/DialogConfirmTransactionLoading.tsx
@@ -1,15 +1,24 @@
 import { DotsSpinnerImg } from "@/public/assets";
-import { Dialog, DialogContent, ModalBaseProps } from "@/ui";
+import { Dialog, DialogContent } from "@/ui";
+import { ReactNode } from "react";
+
+interface DialogConfirmTransactionLoadingProps {
+  isOpen: boolean;
+  closeAction?: () => void;
+  children?: ReactNode;
+}
 
 export const DialogConfirmTransactionLoading = ({
   closeAction,
   isOpen,
-}: ModalBaseProps) => (
-  <Dialog closeAction={closeAction} isOpen={isOpen}>
+  children,
+}: DialogConfirmTransactionLoadingProps) => (
+  <Dialog closeAction={closeAction ? closeAction : () => {}} isOpen={isOpen}>
     <DotsSpinnerImg className="animate-spin" />
     <DialogContent
       description="Confirm this transaction in your wallet."
       title="Waiting for confirmation"
     />
+    {children}
   </Dialog>
 );
