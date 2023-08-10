ea5bde9b1f505ec22e40df467449e7c53e76d567
Diogo Ferreira <diogomf@hey.com>
2023-08-10 11:21:04 +0100

    fix: token type

--- packages/app/models/token/types.ts
+++ packages/app/models/token/types.ts
@@ -1,16 +1,6 @@
 import { TokenSubgraph } from "@stackly/sdk";
 
-type TokenWithoutIdAndAddress = Omit<TokenSubgraph, "id" | "address">;
-
-interface TokenWithAddress extends TokenWithoutIdAndAddress {
-  address: string;
-}
-
-interface TokenWithId extends TokenWithoutIdAndAddress {
-  address: string;
-}
-
-export type Token = TokenWithAddress | TokenWithId | TokenSubgraph;
+export type Token = TokenSubgraph | Omit<TokenSubgraph, "id">;
 
 export interface TokenFromTokenlist {
   address: string;
