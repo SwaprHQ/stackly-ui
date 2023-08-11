d0477b96a1447e76b456cc83f8a60259d250a2dc
Diogo Ferreira <diogomf@hey.com>
2023-08-11 11:54:04 +0100

    feat: Add support for multiple modals open (#44)
    * fix: closeAction conditional was wrong
    
    * feat: add support for multiple modals open
    - update components to use new api
    
    * fix: add isModalOpen to useMemo
    
    * fix: remove unused Toast component

--- packages/app/components/stackbox/Stackbox.tsx
+++ packages/app/components/stackbox/Stackbox.tsx
@@ -2,17 +2,8 @@
 
 import { useRef, useState, useEffect } from "react";
 import { cx } from "class-variance-authority";
-import Link from "next/link";
 
-import {
-  BodyText,
-  Button,
-  Icon,
-  RadioButton,
-  Severity,
-  TitleText,
-  Toast,
-} from "@/ui";
+import { BodyText, Button, Icon, RadioButton, TitleText } from "@/ui";
 import {
   ConfirmStackModal,
   DatePicker,
@@ -63,7 +54,7 @@ export const Stackbox = () => {
   const [isPickingTokenFrom, setIsPickingTokenFrom] = useState<boolean>(false);
   const [tokenFrom, setTokenFrom] = useState<TokenFromTokenlist>();
   const [tokenTo, setTokenTo] = useState<TokenFromTokenlist>();
-  const { closeModal, openModalId, openModal } = useModalContext();
+  const { closeModal, isModalOpen, openModal } = useModalContext();
 
   const { chain } = useNetwork();
   const { address } = useAccount();
@@ -126,7 +117,7 @@ export const Stackbox = () => {
           />
           <Icon
             name="arrow-left"
-            className="flex items-center justify-center p-2 w-16 h-9 bg-surface-50 rounded-2xl rotate-180"
+            className="flex items-center justify-center w-16 p-2 rotate-180 h-9 bg-surface-50 rounded-2xl"
           />
           <SelectTokenButton
             label="To receive"
@@ -139,7 +130,7 @@ export const Stackbox = () => {
             type="number"
             pattern="[0-9]*"
             placeholder="0.0"
-            className="w-full py-3 text-4xl text-em-med font-semibold outline-none"
+            className="w-full py-3 text-4xl font-semibold outline-none text-em-med"
             value={tokenAmount}
             onKeyDown={(evt) =>
               ["e", "E", "+", "-"].includes(evt.key) && evt.preventDefault()
@@ -149,7 +140,7 @@ export const Stackbox = () => {
             }}
           />
           {tokenFrom && balance && (
-            <div className="flex justify-between items-center">
+            <div className="flex items-center justify-between">
               <div className="flex space-x-1">
                 {balanceOptions.map(({ name, divider }) => (
                   <Button
@@ -170,7 +161,7 @@ export const Stackbox = () => {
                   </Button>
                 ))}
               </div>
-              <div className="flex space-x-1 items-center">
+              <div className="flex items-center space-x-1">
                 <TokenIcon token={tokenFrom} size="2xs" />
                 <BodyText className="text-em-high">
                   <span className="text-em-low">Balance:</span>{" "}
@@ -209,7 +200,7 @@ export const Stackbox = () => {
                 );
               })}
             </div>
-            <div className="flex flex-col md:flex-row rounded-2xl border border-surface-50 divide-y md:divide-x divide-surface-50">
+            <div className="flex flex-col border divide-y md:flex-row rounded-2xl border-surface-50 md:divide-x divide-surface-50">
               <div className="flex flex-col w-full px-4 py-3 space-y-2">
                 <BodyText size={2}>Starting from</BodyText>
                 <DatePicker
@@ -237,29 +228,15 @@ export const Stackbox = () => {
         </Button>
       </div>
       <TokenPicker
-        closeAction={closeModal}
+        closeAction={() => closeModal(ModalId.TOKEN_PICKER)}
         initialFocusRef={searchTokenBarRef}
-        isOpen={openModalId === ModalId.TOKEN_PICKER}
+        isOpen={isModalOpen(ModalId.TOKEN_PICKER)}
         onTokenSelect={selectToken}
       />
       <ConfirmStackModal
-        isOpen={openModalId === ModalId.CONFIRM_STACK}
-        closeAction={closeModal}
+        isOpen={isModalOpen(ModalId.CONFIRM_STACK)}
+        closeAction={() => closeModal(ModalId.CONFIRM_STACK)}
       />
-      <Toast
-        closeAction={closeModal}
-        isOpen={openModalId === ModalId.TOAST_CONTAINER}
-        severity={Severity.SUCCESS}
-        title="Your stack creation was successful"
-      >
-        <Link
-          className="flex items-center space-x-0.5 hover:border-em-low border-b-2 border-em-disabled group"
-          href="/stacks"
-          onClick={closeModal}
-        >
-          <BodyText className="text-em-med">View your stacks</BodyText>
-        </Link>
-      </Toast>
     </div>
   );
 };
