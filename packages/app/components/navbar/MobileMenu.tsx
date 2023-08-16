54e672db164a296a37dee7a1a3f4614e834dd06d
Diogo Ferreira <diogomf@hey.com>
2023-08-16 17:30:41 +0100

    update: refactor mobile menu to not hide way simple components

--- packages/app/components/navbar/MobileMenu.tsx
+++ packages/app/components/navbar/MobileMenu.tsx
@@ -1,18 +1,52 @@
 "use client";
 
 import { useState } from "react";
-import MenuButton from "./MenuButton";
-import MobileMenuList from "./MobileMenuList";
 import { ConnectButton, SelectNetwork } from "@/components";
+import Link from "next/link";
+import { Button, Icon } from "@/ui";
 
 export default function MobileMenu() {
   const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <div className="flex items-center justify-end w-full gap-4 md:hidden">
       <SelectNetwork />
       <ConnectButton />
-      <MenuButton open={isOpen} toggle={() => setIsOpen(!isOpen)} />
-      <MobileMenuList open={isOpen} toggle={() => setIsOpen(!isOpen)} />
+      <Button
+        action="secondary"
+        iconLeft={isOpen ? "close" : "menu"}
+        size="icon"
+        className="md:invisible"
+        onClick={toggle}
+      />
+      {isOpen && (
+        <>
+          <div
+            className="fixed bottom-0 left-0 right-0 top-nav-height bg-gray-alpha-75"
+            onClick={toggle}
+          ></div>
+          <div className="absolute left-0 w-full gap-2 px-6 py-2 border-b border-solid rounded-lg bg-surface-25 top-nav-height border-surface-75">
+            <Link
+              passHref
+              href="/stacks"
+              className="flex items-center py-3 text-em-med"
+              onClick={toggle}
+            >
+              <Icon name="blocks" alt="your stacks" size={18} />
+              <span className="ml-4">Your Stacks</span>
+            </Link>
+            <hr className="h-0 -mx-6 border-b border-solid border-surface-75" />
+            <Link href="#" className="block py-3 text-em-med">
+              How it works
+            </Link>
+            <Link href="#" className="block py-3 text-em-med">
+              FAQ&apos;s
+            </Link>
+          </div>
+        </>
+      )}
     </div>
   );
 }
