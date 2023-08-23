3804fba5429fa6135a3b89754a31ec904e933821
Diogo Ferreira <diogomf@hey.com>
2023-08-23 11:06:06 +0100

    fix: outline stroke on radio buttons should be lighter

--- packages/app/ui/buttons/RadioButton.tsx
+++ packages/app/ui/buttons/RadioButton.tsx
@@ -12,24 +12,24 @@ export const radioButtonStyles = cva(
     "cursor-pointer select-none font-medium text-xs",
     "active:ring-4",
     "disabled:bg-surface-75 disabled:text-em-disabled disabled:cursor-not-allowed disabled:ring-0",
-    "focus:outline-none focus:ring-4"
+    "focus:outline-none focus:ring-4",
   ],
   {
     variants: {
       checked: {
         true: [
-          "bg-primary-400 text-em-high hover:bg-primary-500 border border-primary-400 active:ring-primary-200 shadow-xs",
-          "focus:bg-primary-500 focus:ring-primary-200"
+          "bg-primary-400 text-em-high hover:bg-primary-500 border border-primary-400 active:ring-primary-100 shadow-xs",
+          "focus:bg-primary-500 focus:ring-primary-100",
         ],
         false: [
-          "bg-white text-em-med hover:bg-surface-75 border active:ring-gray-100 shadow-xs",
-          "focus:bg-surface-75 focus:ring-gray-100"
-        ]
-      }
+          "bg-white text-em-med hover:bg-surface-75 border active:ring-surface-50 shadow-xs",
+          "focus:bg-surface-75 focus:ring-surface-50",
+        ],
+      },
     },
     defaultVariants: {
-      checked: false
-    }
+      checked: false,
+    },
   }
 );
 
@@ -40,7 +40,7 @@ export function RadioButton({
   value,
   checked,
   name,
-  onChange
+  onChange,
 }: RadioButtonProps) {
   return (
     <div className="flex">
@@ -57,7 +57,7 @@ export function RadioButton({
         htmlFor={id}
         className={radioButtonStyles({
           checked,
-          className
+          className,
         })}
       >
         {children}
