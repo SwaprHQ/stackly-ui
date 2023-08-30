bdd5cc4f867d1ce638b04ab419a0b86132eea6df
Leonardo Berteotti <berteotti2@gmail.com>
2023-08-30 12:00:20 +0100

    feat: add beta NFT modal (#71)

--- packages/app/ui/modal/ModalHeader.tsx
+++ packages/app/ui/modal/ModalHeader.tsx
@@ -3,12 +3,20 @@
 import { PropsWithChildren } from "react";
 
 import { Dialog } from "@headlessui/react";
+import { twMerge } from "tailwind-merge";
 
-export function ModalHeader({ children }: PropsWithChildren) {
+interface ModalHeaderProps extends PropsWithChildren {
+  className?: string;
+}
+
+export function ModalHeader({ children, className }: ModalHeaderProps) {
   return (
     <Dialog.Title
       as="div"
-      className="flex items-center justify-between w-full px-4 py-4 border-b md:px-6 border-surface-50"
+      className={twMerge(
+        "flex items-center justify-between w-full px-4 py-4 border-b md:px-6 border-surface-50",
+        className
+      )}
     >
       {children}
     </Dialog.Title>
