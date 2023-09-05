6c8ddd675838cee34b9927ad94a778a4b68cf2d3
Diogo Ferreira <diogomf@hey.com>
2023-09-05 12:20:12 +0100

    feat: add svg support

--- packages/landing/next.config.js
+++ packages/landing/next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  distDir: "../../dist",
+  reactStrictMode: true,
+  trailingSlash: true,
+  webpack(config) {
+    config.resolve.fallback = { "pino-pretty": false, lokijs: false };
 
-module.exports = nextConfig
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
