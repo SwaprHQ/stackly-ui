240fa5d6bc7072dd9abe2cf87e888e4bdcad04f0
Diogo Ferreira <diogomf@hey.com>
2023-09-11 13:46:09 +0100

    landing: add FAQ section (#94)
    * landing: add DCA section
    
    * landing: add FAQs section
    - add caret up icon
    - change selection bg color

---
+++ packages/landing/components/QAndAAccordion/QAndAAccordion.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Icon } from "@/ui";
+import { cx } from "class-variance-authority";
+import { PropsWithChildren, useState } from "react";
+
+interface QAndAAccordionProps extends PropsWithChildren {
+  question: string;
+}
+
+export const QAndAAccordion = ({ question, children }: QAndAAccordionProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div
+      className={cx(
+        "px-6 py-3 space-y-4 bg-white border cursor-pointer rounded-xl hover:bg-surface-25",
+        {
+          "shadow-md": isOpen,
+          "shadow-sm": !isOpen,
+        }
+      )}
+      onClick={toggle}
+    >
+      <div className="flex items-center justify-between">
+        <p className="text-xl font-semibold">{question}</p>
+        <Icon name={isOpen ? "caret-up" : "caret-down"} />
+      </div>
+      {isOpen && (
+        <div className="space-y-3 text-lg font-medium text-em-med">
+          {children}
+        </div>
+      )}
+    </div>
+  );
+};
