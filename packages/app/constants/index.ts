26a85f9ecf1a101186f9832ff0666cdaa8964f00
Diogo Ferreira <diogomf@hey.com>
2023-09-21 00:03:50 +0100

    fix: add stackly app and landing url missing links as constants

---
+++ packages/app/constants/index.ts
@@ -0,0 +1,5 @@
+export const STACKLY_LANDING_URL =
+  process.env.STACKLY_LANDING_URL ?? "https://stackly.app";
+
+export const STACKLY_APP_URL =
+  process.env.STACKLY_APP_URL ?? "https://stackly.eth.limo";
