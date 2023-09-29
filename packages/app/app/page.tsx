f5035401a76127516e96047825e17d431a0729cf
Rorry <rodrigojali@gmail.com>
2023-09-29 16:36:04 -0300

    feat(STK-185): place Strategies module and wire up with the Stackbox

--- packages/app/app/page.tsx
+++ packages/app/app/page.tsx
@@ -1,9 +1,10 @@
-import { Stackbox } from "@/components";
+import { Stackbox, Strategies } from "@/components";
 
 export default function Home() {
   return (
     <div className="w-full mx-auto">
       <Stackbox />
+      <Strategies />
     </div>
   );
 }
