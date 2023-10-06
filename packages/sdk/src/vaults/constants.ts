accc50c9f642776a65bc64d3d14fc9e84b86cc53
Rorry <rodrigojali@gmail.com>
2023-10-06 11:40:41 -0300

    feat(STK-191): update SDK package for Mainnet support

--- packages/sdk/src/vaults/constants.ts
+++ packages/sdk/src/vaults/constants.ts
@@ -1,24 +1,43 @@
 import { AddressZero } from "@ethersproject/constants";
 import { ChainId } from "../constants";
 
+export const GNOSIS_ORDER_FACTORY_ADDRESS =
+  "0x45B91Da2834010751b17F1eadE0a5a7B64233add";
+
+export const MAINNET_ORDER_FACTORY_ADDRESS =
+  "0x8b1a70feccc6c2ae6017ddd8d0bebdd1bd6eb261";
+
+const validateVaultInfo = (
+  chainId: ChainId,
+  map: Record<ChainId, string> | Readonly<Record<string, string>>,
+  mapName: string
+) => {
+  const address = map[chainId];
+  if (!address || address === AddressZero) {
+    throw new Error(`${mapName} is not deployed on chain ${chainId}`);
+  }
+
+  return address;
+};
+
 /**
  * Order factory address list
  */
 export const ORDER_FACTORY_ADDRESS_LIST: Record<ChainId, string> = {
-  [ChainId.ETHEREUM]: AddressZero,
-  [ChainId.GNOSIS]: "0x45B91Da2834010751b17F1eadE0a5a7B64233add",
+  [ChainId.ETHEREUM]: MAINNET_ORDER_FACTORY_ADDRESS,
+  [ChainId.GNOSIS]: GNOSIS_ORDER_FACTORY_ADDRESS,
 };
 
 /**
  * DCA Order singleton/mastercopy address list
  */
 export const DCAORDER_SINGLETON_ADDRESS_LIST: Record<ChainId, string> = {
-  [ChainId.ETHEREUM]: AddressZero,
+  [ChainId.ETHEREUM]: "0xc97ecbdba20c672c61e27bd657d4dfbd2328f6fa",
   [ChainId.GNOSIS]: "0x521aC6dA724aD873977d3d0b5d7B4a52E1dC52b7",
 };
 
 /**
- * COW's settlement address list
+ * COW's settlement address list, CoW Protcol GPv2VaultRelayer
  */
 export const COW_SETTLEMENT_ADDRESS_LIST: Record<ChainId, string> = {
   [ChainId.ETHEREUM]: "0xC92E8bdf79f0507f65a392b0ab4667716BFE0110",
@@ -26,7 +45,8 @@ export const COW_SETTLEMENT_ADDRESS_LIST: Record<ChainId, string> = {
 };
 
 export const SUBGRAPH_ENDPOINT_LIST: Readonly<Record<string, string>> = {
-  [ChainId.ETHEREUM]: "",
+  [ChainId.ETHEREUM]:
+    "https://api.thegraph.com/subgraphs/name/swaprhq/stackly-ethereum",
   [ChainId.GNOSIS]: "https://api.thegraph.com/subgraphs/name/swaprhq/stackly",
 };
 
@@ -44,12 +64,11 @@ export const NFT_WHITELIST_ADDRESS_LIST: Record<ChainId, string> = {
  * @returns
  */
 export function getOrderFactoryAddress(chainId: ChainId): string {
-  const address = ORDER_FACTORY_ADDRESS_LIST[chainId];
-  if (address === AddressZero) {
-    throw new Error(`Order factory is not deployed on chain ${chainId}`);
-  }
-
-  return address;
+  return validateVaultInfo(
+    chainId,
+    ORDER_FACTORY_ADDRESS_LIST,
+    "Order factory"
+  );
 }
 
 /**
@@ -58,12 +77,11 @@ export function getOrderFactoryAddress(chainId: ChainId): string {
  * @returns
  */
 export function getDCAOrderSingletonAddress(chainId: ChainId): string {
-  const address = DCAORDER_SINGLETON_ADDRESS_LIST[chainId];
-  if (address === AddressZero) {
-    throw new Error(`DCAOrder singleton is not deployed on chain ${chainId}`);
-  }
-
-  return address;
+  return validateVaultInfo(
+    chainId,
+    DCAORDER_SINGLETON_ADDRESS_LIST,
+    "DCAOrder singleton"
+  );
 }
 
 /**
@@ -72,10 +90,11 @@ export function getDCAOrderSingletonAddress(chainId: ChainId): string {
  * @returns The address of the settlement contract
  */
 export function getCOWProtocolSettlementAddress(chainId: ChainId): string {
-  return {
-    [ChainId.ETHEREUM]: "0x9008D19f58AAbD9eD0D60971565AA8510560ab41",
-    [ChainId.GNOSIS]: "0x9008d19f58aabd9ed0d60971565aa8510560ab41",
-  }[chainId];
+  return validateVaultInfo(
+    chainId,
+    COW_SETTLEMENT_ADDRESS_LIST,
+    "CoW Settlement"
+  );
 }
 
 /**
@@ -85,13 +104,7 @@ export function getCOWProtocolSettlementAddress(chainId: ChainId): string {
  * @throws Error if no subgraph endpoint is found for the chainId
  */
 export function getSubgraphEndpoint(chainId: ChainId) {
-  const endpoint = SUBGRAPH_ENDPOINT_LIST[chainId];
-
-  if (!endpoint || endpoint === "") {
-    throw new Error(`No subgraph endpoint for chainId ${chainId}`);
-  }
-
-  return endpoint;
+  return validateVaultInfo(chainId, SUBGRAPH_ENDPOINT_LIST, "Subraph Endpoint");
 }
 
 /**
