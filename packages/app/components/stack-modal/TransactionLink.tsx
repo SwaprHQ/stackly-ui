77a46c7ce41e24a9066009731135dd129b277db9
Rorry <rodrigojali@gmail.com>
2023-10-09 12:00:45 -0300

    feat(STK-191): add mainnet to the TransactionLink explorer lists

--- packages/app/components/stack-modal/TransactionLink.tsx
+++ packages/app/components/stack-modal/TransactionLink.tsx
@@ -2,10 +2,10 @@ import { txEplorerLink } from "@/utils/transaction";
 import { Icon } from "@/ui";
 import { Transaction } from "@/models/stack";
 
-export const TransactionLink = ({ hash }: Transaction) => (
+export const TransactionLink = ({ chainId, hash }: Transaction) => (
   <a
     className="flex items-center text-primary-100 hover:underline hover:underline-offset-4"
-    href={txEplorerLink(hash)}
+    href={txEplorerLink(chainId, hash)}
     target="blank"
   >
     Check Transaction <Icon size={16} name="arrow-external" className="ml-1" />
--- packages/app/utils/transaction.ts
+++ packages/app/utils/transaction.ts
@@ -1,1 +1,12 @@
-export const txEplorerLink = (tx: string) => `https://gnosisscan.io/tx/${tx}`;
+import { ChainId } from "@stackly/sdk";
+
+const EXPLORER_URL_BY_CHAIN = {
+  1: "https://etherscan.io",
+  100: "https://gnosisscan.io",
+};
+
+export const txEplorerLink = (chainId: ChainId, txHash: string) => {
+  const baseUrl = EXPLORER_URL_BY_CHAIN[chainId];
+
+  return `${baseUrl}/tx/${txHash}`;
+};
