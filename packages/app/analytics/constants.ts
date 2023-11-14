7d78ab412c539e109dc1f1e8adbb37d33ca0b4bc
Rorry <rodrigojali@gmail.com>
2023-11-14 13:48:11 -0300

    infra(STK-212): create new FA events to track the stack creation flow

--- packages/app/analytics/constants.ts
+++ packages/app/analytics/constants.ts
@@ -4,4 +4,10 @@ export const EVENTS = {
   CLICK: {
     STRATEGY_CARD: "click/strategy-card",
   },
+  CREATE_FLOW: {
+    CONFIRM_STACK_MODAL_STACK_NOW_CLICK:
+      "createFlow/confirm-stack-modal-stack-now-click",
+    STACK_CREATION_IN_PROGRESS: "createFlow/stack-creation-in-progress",
+    STACKBOX_CONFIRM_STACK_CLICK: "createFlow/stackbox-confirm-stack-click",
+  },
 };
