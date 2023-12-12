d285f265f013e5b5eee3beb12d1d181ab046eb41
Rorry <rodrigojali@gmail.com>
2023-12-12 11:14:15 -0300

    prune(STK-218): PR comment to remove unneeded files

--- packages/app/utils/index.ts
+++ packages/app/utils/index.ts
@@ -3,6 +3,5 @@ export * from "./constants";
 export * from "./datetime";
 export * from "./ethers";
 export * from "./numbers";
-export * from "./strings";
 export * from "./token";
 export * from "./transaction";
