170f3c2fff20d8b60c880090a3a7abd4bd423884
Berteotti <berteotti2@gmail.com>
2024-08-27 16:50:58 +0100

    fix: subgraph api key

--- packages/sdk/src/vaults/constants.ts
+++ packages/sdk/src/vaults/constants.ts
@@ -57,8 +57,9 @@ export const COW_SETTLEMENT_ADDRESS_LIST: Record<ChainId, string> = {
 };
 
 const API_BASE_URL = "https://gateway-arbitrum.network.thegraph.com/api";
-const SUBGRAPH_API_KEY = process.env.STACKLY_SUBGRAPH_API_KEY ?? "";
 
+const SUBGRAPH_API_KEY =
+  process.env.STACKLY_SUBGRAPH_API_KEY ?? "e7b7ff845e506590498946cd6bf83bf6";
 const ETHEREUM_SUBGRAPH_ENDPOINT_URL =
   process.env.ETHEREUM_SUBGRAPH_API_URL ??
   `${API_BASE_URL}/${SUBGRAPH_API_KEY}/subgraphs/id/7s9rGBffUTL8kDZuxvvpuc46v44iuDarbrADBFw5uVp2`;
